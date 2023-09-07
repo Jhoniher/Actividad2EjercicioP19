58bfc8f9968449f87658315f452e497b80c811f6
Jhony Alpala <jhoniher95@gmail.com>
2023-09-07 09:34:10 -0500

    Calculos realizados

--- EjercicioPropuesto19.java
+++ EjercicioPropuesto19.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
 public class EjercicioPropuesto19 {
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+        double lado;
+        System.out.println("Ingrese el valor del lado del triangulo equilatero: ");
+        lado=entrada.nextDouble();
+        double perimetro=lado+lado+lado;
+        double altura=(Math.sqrt(3)/2)*lado;
+        double area=(lado*altura)/2;
+    }
 }
